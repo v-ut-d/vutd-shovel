772fab878d792f833a829fbd9557de1f585ebc7a
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-01-22 23:14:54 +0900

    add commands in handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,16 @@
 import { Client, Interaction } from 'discord.js';
+import * as commands from './commands';
 
-export function interaction(interaction: Interaction) {
+export async function interaction(interaction: Interaction) {
   if (!interaction.inCachedGuild()) return;
+  try {
+    if (interaction.isCommand()) await commands.handle(interaction);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-export function ready(client: Client<true>) {
+export async function ready(client: Client<true>) {
   console.log(`logged in as ${client.user.tag}`);
+  await commands.register(client);
 }
