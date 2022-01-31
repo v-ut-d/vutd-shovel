d3360262045839364dfb538190ad686c84994811
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-02-01 00:49:31 +0900

    preprocess unicode emojis

--- src/classes/preprocesser.ts
+++ src/classes/preprocesser.ts
@@ -1,6 +1,35 @@
 import { normalizeDiacritics } from 'normalize-text';
 import { Room } from '.';
 import alkana from '../../alkana.json';
+import emoji from '../../emoji.json';
+
+const TO_BE_ESCAPED = '\\*+.?{}()[]^$-|/';
+
+const GUILD_EMOJI_REPLACER = [/<:(.+?):\d{18}>/g, ':$1:'] as const;
+
+const ENGLISH_WORD_REPLACER = [
+  /([a-z]+) ?/gi,
+  // 'as' assertion; `str in alkana` guarantees this
+  (_: unknown, str: string) =>
+    str in alkana ? alkana[str as keyof typeof alkana] : str,
+] as const;
+
+const UNICODE_EMOJI_REPLACER = [
+  new RegExp(
+    Object.keys(emoji)
+      .map((s) => (TO_BE_ESCAPED.includes(s) ? `\\${s}` : s))
+      .join('|'),
+    'g'
+  ),
+  // 'as' assertion; regexp above guarantees this
+  (str: string) => emoji[str as keyof typeof emoji],
+] as const;
+
+const URL_REPLACER = [
+  // eslint-disable-next-line no-irregular-whitespace
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[^\s　]*)?/g,
+  'URL省略\n',
+] as const;
 
 /**
  * Preprocesser that is used before Open JTalk synthesizes voice.
@@ -14,15 +43,9 @@ export default class Preprocesser {
    */
   exec(content: string): string {
     return normalizeDiacritics(content)
-      .replace(/<:(.+?):\d{18}>/g, ':$1:')
-      .replace(/[a-z]+/gi, (str) =>
-        // 'as' assertion; `str in alkana` guarantees this
-        str in alkana ? alkana[str as keyof typeof alkana] : str
-      )
-      .replace(
-        // eslint-disable-next-line no-irregular-whitespace
-        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[^\s　]*)?/g,
-        'URL省略\n'
-      );
+      .replace(...GUILD_EMOJI_REPLACER)
+      .replace(...ENGLISH_WORD_REPLACER)
+      .replace(...UNICODE_EMOJI_REPLACER)
+      .replace(...URL_REPLACER);
   }
 }
