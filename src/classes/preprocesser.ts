70b384dda89375545aca7d2acc325ad8abddcf87
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-01-31 23:25:35 +0900

    add normalize-text

--- src/classes/preprocesser.ts
+++ src/classes/preprocesser.ts
@@ -1,3 +1,4 @@
+import { normalizeDiacritics } from 'normalize-text';
 import { Room } from '.';
 
 /**
@@ -11,10 +12,12 @@ export default class Preprocesser {
    * preprocesses the string.
    */
   exec(content: string): string {
-    return content.replace(/<:(.+?):\d{18}>/g, ':$1:').replace(
-      // eslint-disable-next-line no-irregular-whitespace
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[^\s　]*)?/g,
-      'URL省略\n'
-    );
+    return normalizeDiacritics(content)
+      .replace(/<:(.+?):\d{18}>/g, ':$1:')
+      .replace(
+        // eslint-disable-next-line no-irregular-whitespace
+        /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}(\/[^\s　]*)?/g,
+        'URL省略\n'
+      );
   }
 }
