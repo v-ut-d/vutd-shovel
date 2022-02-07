cfb824c3eda9f95dec0a31362887eb276fdf925d
femshima <49227365+femshima@users.noreply.github.com>
2022-02-07 18:10:04 +0900

    [update]better cleaning

--- script/dict.sh
+++ script/dict.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 mkdir -p dictionary/tmp
-cd dictionary/tmp
+cd dictionary
 rm -r ./*
+cd tmp
 
 cp -r ../../node_modules/node-openjtalk-binding/openjtalk/mecab-naist-jdic/* ./
 curl -L -o tdmelodic_openjtalk.csv https://github.com/sarulab-speech/tdmelodic_openjtalk/raw/main/tdmelodic_openjtalk.csv
 
 npx mecab-dict-index -d ./ -o ../
 
-cp char.bin matrix.bin sys.dic unk.dic ../
