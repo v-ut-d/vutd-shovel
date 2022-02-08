438269084b10ddefba345bccb88dc1ab327fe5bd
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-02-08 17:11:24 +0900

    styling

--- src/classes/preprocessor.test.ts
+++ src/classes/preprocessor.test.ts
@@ -9,6 +9,7 @@ const pillow =
 
 describe('Test Preprocessor', () => {
   const preprocessor = new Preprocessor(new RoomMock());
+
   it('Replace basic URLs', () => {
     expect(preprocessor.exec('http://example.com')).toBe('URL省略\n');
     expect(preprocessor.exec('https://example.com/')).toBe('URL省略\n');
@@ -22,21 +23,25 @@ describe('Test Preprocessor', () => {
       'hogeURL省略\n\nfuga'
     );
   });
+
   it('Do not replace other protocols', () => {
     expect(preprocessor.exec('ftp://example.com')).toBe(
       'ftp:スラッシュスラッシュイグザンプル.コム'
     );
   });
+
   it('Replace Japanese URLs', () => {
     expect(preprocessor.exec('http://日本語.com/')).toBe('URL省略\n');
     expect(preprocessor.exec('http://xn--wgv71a119e.com/')).toBe('URL省略\n');
   });
+
   it('Replace GUILD Emojis', () => {
     expect(preprocessor.exec('<:emoji:618391439964133143>')).toBe(':emoji:');
     expect(preprocessor.exec('This is an <:E:618391439964133143>moji')).toBe(
       'Thisイズアン:E:moji'
     );
   });
+
   it('Do not replace non-guild-emojis', () => {
     expect(preprocessor.exec('<:emojiR:18391439964133143>')).toBe(
       '小なり:emojiR:18391439964133143大なり'
@@ -51,6 +56,7 @@ describe('Test Preprocessor', () => {
       '小なり:emojiR:618391439964133143'
     );
   });
+
   it('Replace WARA', () => {
     expect(preprocessor.exec('w')).toBe('わら');
     expect(preprocessor.exec('わらw')).toBe('わらわら');
@@ -59,10 +65,12 @@ describe('Test Preprocessor', () => {
     expect(preprocessor.exec('わらwwwwwwwwwwwwww')).toBe('わらわらわら');
     expect(preprocessor.exec('わら\nw')).toBe('わら\nわら');
   });
+
   it('Do not replace trap-WARA', () => {
     expect(preprocessor.exec('woow')).toBe('woow');
     expect(preprocessor.exec('whatw')).toBe('whatw');
   });
+
   it('Replace too long sentence', () => {
     expect(preprocessor.exec(pillow.substring(0, 100))).toBe(
       pillow.substring(0, 100)
@@ -71,6 +79,7 @@ describe('Test Preprocessor', () => {
       pillow.substring(0, 100) + '\n以下略'
     );
   });
+
   it('Replace too long sentence(with URL)', () => {
     const url = 'http://example.com ';
     expect(preprocessor.exec(url + pillow.substring(0, 93))).toBe(
