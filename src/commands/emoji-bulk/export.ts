941f02ddb5bb74d5fd1119205dc01bb1c46b6fc4
CallMe AsYouFeel <75649254+cm-ayf@users.noreply.github.com>
2022-02-10 17:35:32 +0900

    simplify implementation

--- src/commands/emoji-bulk/export.ts
+++ src/commands/emoji-bulk/export.ts
@@ -20,50 +20,28 @@ export const data: ApplicationCommandSubCommandData = {
  */
 export async function handle(interaction: CommandInteraction<'cached'>) {
   try {
-    const emojis = await prisma.emoji.findMany({
-      where: {
-        guildId: interaction.guildId,
-      },
+    const [emojisAll, emojisDb] = await Promise.all([
+      interaction.guild.emojis.fetch(),
+      prisma.emoji.findMany({
+        where: {
+          guildId: interaction.guildId,
+        },
+      }),
+    ]);
+
+    const rows: string[] = [];
+
+    emojisDb.forEach((emojiDb) => {
+      const emojiDiscord = emojisAll.get(emojiDb.emojiId);
+      if (emojiDiscord) rows.push(`${emojiDiscord}, ${emojiDb.pronounciation}`);
     });
 
-    const emojiMap: Map<
-      string,
-      {
-        id: string;
-        pronounciation: string;
-        emojiName: string | null;
-        animated: boolean | null;
-      }
-    > = new Map();
-    emojis.forEach((emoji) => {
-      emojiMap.set(emoji.emojiId, {
-        id: emoji.emojiId,
-        pronounciation: emoji.pronounciation,
-        emojiName: null,
-        animated: null,
-      });
-    });
-    (await interaction.guild.emojis.fetch()).forEach((emoji) => {
-      const emojiobj = emojiMap.get(emoji.id);
-      if (emojiobj) {
-        emojiobj.emojiName = emoji.name;
-        emojiobj.animated = emoji.animated;
-      }
-    });
-
-    const csv = Array.from(emojiMap.values())
-      .map<string>(
-        (v) =>
-          `<${v.animated ? 'a' : ''}:${v.emojiName}:${v.id}>,${
-            v.pronounciation
-          }`
-      )
-      .join('\n');
+    const csv = rows.join('\n');
     const fileName = `dictionary/${interaction.guildId}_export.dict`;
     fs.writeFileSync(fileName, csv, 'utf-8');
 
     await interaction.reply({
-      embeds: [new EmojiBulkMessageEmbed('export', emojis.length)],
+      embeds: [new EmojiBulkMessageEmbed('export', rows.length)],
       files: [fileName],
     });
 
