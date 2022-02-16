775184d37ebb1c5c15d1caca476f769c80bde888
femshima <49227365+femshima@users.noreply.github.com>
2022-02-17 08:02:56 +0900

    [add]Implement multi-guild/Production mode support

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,28 +1,120 @@
-import type { Client, CommandInteraction } from 'discord.js';
-import { getGuild } from '../utils';
+import type { GuildSettings } from '@prisma/client';
+import type {
+  ApplicationCommand,
+  ApplicationCommandPermissions,
+  Client,
+  CommandInteraction,
+  Snowflake,
+} from 'discord.js';
+import { Collection } from 'discord.js';
+import { env } from '../utils';
 import * as cancel from './cancel';
-import * as emojiBulk from './emoji-bulk';
+import * as dict from './dict';
 import * as dictBulk from './dict-bulk';
+import * as emojiBulk from './emoji-bulk';
 import * as end from './end';
 import * as start from './start';
 import * as voice from './voice';
-import * as dict from './dict';
+
+const commands = { start, end, cancel, voice, dict, emojiBulk, dictBulk };
+
+type CommandNames = keyof typeof commands;
+
+//Value: ApplicationCommand(in production) or Collection of ApplicationCommand with guildId as key(in development)
+type ApplicationCommands = Record<
+  CommandNames,
+  ApplicationCommand | Collection<Snowflake, ApplicationCommand>
+>;
+
+export type PermissionSetterFunction = (
+  guildSettings: GuildSettings
+) => ApplicationCommandPermissions[];
 
 /**
  * registers slash commands.
  */
 export async function register(client: Client<true>) {
-  const guild = await getGuild(client);
+  const appCommands: Partial<ApplicationCommands> = {};
+
+  const oauth2guilds = await client.guilds.fetch();
+  const guilds = env.production
+    ? undefined
+    : await Promise.all(
+        oauth2guilds.map((guild_partial) => guild_partial.fetch())
+      );
+
+  //Register commands
   await Promise.all(
-    [start, end, cancel, voice, dict, emojiBulk, dictBulk].map((e) =>
-      guild.commands
-        .create(e.data)
-        .then((command) => command.permissions.add(e))
-    )
+    Object.keys(commands).map(async (e) => {
+      if (env.production) {
+        const created = await client.application.commands.create(
+          commands[e as CommandNames].data
+        );
+        appCommands[e as CommandNames] = created;
+      } else {
+        if (!guilds) {
+          //This should not happen.
+          return;
+        }
+        const coll = (appCommands[e as CommandNames] = new Collection<
+          Snowflake,
+          ApplicationCommand
+        >());
+        await Promise.all(
+          guilds.map(async (guild) => {
+            const created = await guild.commands.create(
+              commands[e as CommandNames].data
+            );
+            coll.set(guild.id, created);
+          })
+        );
+      }
+    })
   );
+
+  //Set Command Permissions
+  await Promise.all(
+    oauth2guilds.map(async (guild_partial) => {
+      //TODO: fetch guild permission settings
+      await Promise.all(
+        Object.keys(appCommands).map(async (e) => {
+          const c = appCommands[e as CommandNames];
+          if (!c) return;
+          if ('id' in c) {
+            //Production: ApplicationCommand
+            await client.application.commands.permissions.set({
+              guild: guild_partial.id,
+              command: c,
+              permissions: commands[e as CommandNames].permissions,
+            });
+          } else {
+            //Development: Collection<Snowflake, ApplicationCommand>
+            await Promise.all(
+              c.map(async (appCommand) => {
+                await appCommand.permissions.set({
+                  permissions: commands[e as CommandNames].permissions,
+                });
+              })
+            );
+          }
+        })
+      );
+    })
+  );
+
   console.log('command initialized.');
   process.on('SIGINT', async () => {
-    await guild.commands.set([]).catch(console.error);
+    if (!env.production) {
+      if (!guilds) {
+        //This should not happen.
+        return;
+      }
+      await Promise.all(
+        guilds.map(async (guild) => {
+          await guild.commands.set([]);
+        })
+      );
+    }
     process.exit(0);
   });
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
 import readenv from '@cm-ayf/readenv';
-import type { Base, Client, Guild } from 'discord.js';
 
 if (process.env.NODE_ENV !== 'production')
   // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -10,7 +9,6 @@ if (process.env.NODE_ENV !== 'production')
  */
 export const env = readenv({
   BOT_TOKEN: {},
-  GUILD_ID: {},
   MANAGE_ID: {},
   production: {
     from: 'NODE_ENV',
@@ -18,15 +16,3 @@ export const env = readenv({
     parse: (s) => s === 'production',
   },
 });
-
-function getClient(base: Base | Client) {
-  return 'client' in base ? base.client : base;
-}
-
-/**
- * gets {@link Guild} with env.GUILD_ID.
- * @param base almost any discord.js class fits here.
- */
-export async function getGuild(base: Base | Client): Promise<Guild> {
-  return getClient(base).guilds.fetch(env.GUILD_ID);
-}
