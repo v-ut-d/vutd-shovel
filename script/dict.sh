6b6dc6abbb4eb391b998e2d71e5c6804052e53e7
femshima <49227365+femshima@users.noreply.github.com>
2022-02-28 20:54:34 +0900

    [fix]omit tdmelodic_openjtalk dictionary

--- script/dict.sh
+++ script/dict.sh
@@ -5,7 +5,7 @@ mkdir -p dictionary/tmp
 cd dictionary/tmp
 
 cp -r ../../node_modules/node-openjtalk-binding/openjtalk/mecab-naist-jdic/* ./
-curl -L -o tdmelodic_openjtalk.csv https://github.com/sarulab-speech/tdmelodic_openjtalk/raw/main/tdmelodic_openjtalk.csv
+#curl -L -o tdmelodic_openjtalk.csv https://github.com/sarulab-speech/tdmelodic_openjtalk/raw/main/tdmelodic_openjtalk.csv
 
 npx mecab-dict-index -d ./ -o ../
 
