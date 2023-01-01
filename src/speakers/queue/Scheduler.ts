95b43591f513666ff190743fa65fba80d96bbbe0
femshima <49227365+femshima@users.noreply.github.com>
2023-01-01 21:00:58 +0900

    concat audio if possible

--- src/speakers/queue/Scheduler.ts
+++ src/speakers/queue/Scheduler.ts
@@ -4,13 +4,12 @@ import {
   StreamType,
   AudioPlayerStatus,
 } from '@discordjs/voice';
-import { Collection } from 'discord.js';
 import { silenceOnError } from 'node-openjtalk-binding-discordjs';
 import { Readable } from 'stream';
 import { env } from '../../utils';
+import AudioQueue, { QueueItem } from './AudioQueue';
 
 export type AudioData = { data: Readable; streamType: StreamType };
-type BufferdAudioData = { data?: Buffer; streamType: StreamType };
 export type Executor = () => Promise<AudioData>;
 
 interface Chunk {
@@ -20,7 +19,7 @@ interface Chunk {
 
 export default class Scheduler {
   #synthesisQueue: Chunk[] = [];
-  #playQueue: Collection<number, BufferdAudioData> = new Collection();
+  #playQueue = new AudioQueue();
   #player: AudioPlayer;
   #synthseq = 0;
   #playseq = 0;
@@ -64,47 +63,26 @@ export default class Scheduler {
     const data = await chunk.executor();
     data.data.once('data', release);
 
-    const buf = await this.#streamToBuffer(silenceOnError(data.data));
-    this.#playQueue.set(chunk.sequence, {
-      data: buf,
-      streamType: data.streamType,
-    });
+    this.#playQueue.add(
+      chunk.sequence,
+      silenceOnError(data.data),
+      data.streamType
+    );
     this.#play();
   }
 
   async #play() {
     if (this.#player.state.status === AudioPlayerStatus.Idle) {
-      const data = this.#playQueue.get(this.#playseq);
+      const data = this.#playQueue.get();
       if (!data) return;
-      //console.log('Play:', this.#playseq, '/', Array.from(this.#playQueue.keys()));
-      this.#playQueue.delete(this.#playseq);
+
       this.#doPlay(data);
-      this.#playseq++;
     }
   }
-  #doPlay(data: BufferdAudioData) {
-    if (!data.data) return;
+  #doPlay(data: QueueItem) {
     const resource = createAudioResource(Readable.from(data.data), {
-      inputType: data?.streamType ?? StreamType.Raw,
+      inputType: data.type,
     });
     this.#player.play(resource);
-  }
-
-  #streamToBuffer(stream: Readable): Promise<Buffer> {
-    return new Promise((resolve) => {
-      const buffers: Uint8Array[] = [];
-      stream.on('readable', () => {
-        for (;;) {
-          const buffer = stream.read();
-          if (!buffer) {
-            break;
-          }
-          buffers.push(buffer);
-        }
-      });
-      stream.on('end', () => {
-        resolve(Buffer.concat(buffers));
-      });
-    });
   }
 }
